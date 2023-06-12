b87dbfa25293d48a0773f5a2a6c8654ec6cc1654
simplysabir <simplysabir@gmail.com>
2023-06-12 13:14:19 +0530

    ✨ fix : Hero Section Updated

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <main className='font-mono flex items-center justify-center'>
+        <div className='mt-6 p-6 flex flex-col gap-2'>
+        <h1 className='text_gray_gradient font-bold'>get the latest crypto news at your screen</h1>
+        <Link href={'/'} className='px-2 py-1 bg-slate-400 text-white w-28 text-center'><span>Explore</span></Link>
+        </div>
+    </main>
   )
 }
 
