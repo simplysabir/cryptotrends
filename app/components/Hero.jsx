db359c74b9fa916a62a5ba7e6c91a0ce3779f115
simplysabir <simplysabir@gmail.com>
2023-06-12 16:51:33 +0530

    🧨 fix : Link Connecetd and Working

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <main className='font-mono flex items-center justify-center'>
         <div className='mt-6 p-6 flex flex-col gap-2'>
         <h1 className='text_gray_gradient font-bold'>get the latest crypto news at your screen</h1>
-        <Link href={'/'} className='px-2 py-1 bg-slate-400 text-white w-28 text-center rounded-md'><span>Explore</span></Link>
+        <Link href={'#latest_news'} className='px-2 py-1 bg-slate-400 text-white w-28 text-center rounded-md'><span>Explore</span></Link>
         </div>
     </main>
   )
--- app/components/LatestNews.jsx
+++ app/components/LatestNews.jsx
@@ -3,7 +3,7 @@ import NewsCard from './NewsCard';
 
 const LatestNews = () => {
   return (
-    <section className='mt-10 flex items-center flex-col'>
+    <section className='mt-10 flex items-center flex-col' id='latest_news'>
       <div className='font-mono font-bold section_heading'>
         <h1 className='text-center px-3 py-1  bg-slate-400 text-white rounded-lg'>Latest News</h1>
       </div>
