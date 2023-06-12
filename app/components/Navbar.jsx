0683321d699f4d0b7cb4145f60d7c415068fca11
simplysabir <simplysabir@gmail.com>
2023-06-12 12:49:01 +0530

    ✨ fix : Navbar Added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex items-center justify-between p-4 font-bold font-mono pl-4 pr-4 mb-4'>
+      <h3 className='font-bold'>Crypto Trends</h3>
+      <Link href={'https://github.com/simplysabir/cryptotrends'} target='_blank'>Github</Link>
+    </nav>
   )
 }
 
