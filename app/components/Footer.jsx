ad909b75d5906e99b9b6f89b8cf36968c8c2bc72
simplysabir <simplysabir@gmail.com>
2023-06-12 17:10:26 +0530

    ⚒️ feat : Navbar Completed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='font-mono flex items-start justify-center'>
+        <div className='flex items-center gap-5'>
+            <Link href={'https://twitter.com/simplysabir_'} target='_blank'>Twitter</Link>
+            <Link href={'https://github.com/simplysabir'} target='_blank'>Github</Link>
+            <Link href={'https://linkedin.com/in/simplysabir'} target='_blank'>LinkedIn</Link>
+        </div>
+    </footer>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,15 @@ import Image from 'next/image'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import LatestNews from './components/LatestNews'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <LatestNews />
+      {/* <LatestNews /> */}
+      <Footer/>
     </>
   )
 }     
